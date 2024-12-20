f3b5b164925448a251711f77dbcba60efde24389
DevelopERPNext <develop.erpnext@gmail.com>
2024-12-20 16:09:32 +0200

    first commit 03  -- Adjust Item Price based on Foreign Purchases with Average Purchase Price profit margin

---
+++ adjust_the_price_of_the_item_in_sales_test_001/adjust_the_price_of_the_item_in_sales_test_001/adjust_the_price_of_the_item_in_sales_py.py
@@ -0,0 +1,118 @@
+# Copyright (c) 2024, Mahmoud Khattab
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+from frappe import _
+
+from frappe.utils import nowdate
+
+
+import json
+from frappe.utils import flt
+
+
+from frappe.exceptions import ValidationError
+
+
+@frappe.whitelist()
+def create_print_msg(doc, method=None):
+    frappe.msgprint(str("Adjust Item Price based on Foreign Purchases with Average Purchase Price profit margin"), alert=True)
+    # doc.reload()
+
+
+
+
+
+
+@frappe.whitelist()
+def adjust_item_price_based_on_foreign_purchases_with_average_purchase_price_profit_margin(doc, method=None):
+    """
+    Adjust Item Price based on Foreign Purchases with Average Purchase Price and Profit Margin.
+    This function is triggered on Submit of the Purchase Invoice.
+    """
+    for item in doc.items:
+        item_code = item.item_code
+        uom = item.uom
+
+        # Fetch Item document
+        item_doc = frappe.get_doc("Item", item_code)
+
+        if item_doc.foreign_purchases_items_a_001:
+            profit_margin = flt(item_doc.profit_margin_percentage_items_a_001)
+
+            avg_purchase_price = calculate_average_purchase_price(item_code)
+
+            if avg_purchase_price is not None:
+                new_price = avg_purchase_price * (1 + profit_margin / 100)
+
+                item_price_name = frappe.db.get_value("Item Price", {
+                    "item_code": item_code,
+                    "uom": uom,
+                    "selling": 1
+                }, "name")
+
+                if item_price_name:
+                    item_price_doc = frappe.get_doc("Item Price", item_price_name)
+                    # item_price_doc.price_list_rate = new_price
+
+                    if hasattr(item_price_doc, 'selling') and item_price_doc.selling == 1:
+                        item_price_doc.price_list_rate = new_price
+
+                    item_price_doc.save(ignore_permissions=True)
+                else:
+                    new_item_price = frappe.new_doc("Item Price")
+                    new_item_price.item_code = item_code
+                    new_item_price.uom = uom
+                    new_item_price.price_list_rate = new_price
+                    # new_item_price.price_list = "Standard Selling"
+
+                    if "Standard Selling" in ["Standard Selling", "البيع القياسية"]:
+                        new_item_price.price_list = "Standard Selling"
+                    else:
+                        new_item_price.price_list = "البيع القياسية"
+
+                    new_item_price.save(ignore_permissions=True)
+
+                frappe.msgprint(
+                    f"Item Price for {item_code} has been updated to {new_price:.2f}.",
+                    alert=True
+                )
+
+
+def calculate_average_purchase_price(item_code):
+    """
+    Calculate the average purchase price based on previous Purchase Invoices for the given item code.
+    """
+    purchase_invoices = frappe.db.sql(
+        """
+        SELECT
+            AVG(base_net_rate) AS avg_rate
+        FROM
+            `tabPurchase Invoice Item`
+        WHERE
+            item_code = %s
+        AND
+            docstatus = 1
+        """,
+        (item_code,), as_dict=True
+    )
+
+    # if purchase_invoices and purchase_invoices[0].avg_rate:
+    #     return flt(purchase_invoices[0].avg_rate)
+
+
+    if purchase_invoices and purchase_invoices[0].avg_rate is not None:
+        avg_price = flt(purchase_invoices[0].avg_rate)
+        # frappe.msgprint(f"Average Purchase Price for item {item_code}: {avg_price}")
+        return avg_price
+
+    # frappe.msgprint(f"No purchase data found for item code: {item_code}.")
+    # frappe.msgprint(str("========================"))
+    # frappe.msgprint(frappe.as_json(purchase_invoices))
+
+    return None
+
+
+
+
